23c7d020fcce9df7a22293501ecb853b71c50ab6
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-09 14:50:26 -0600

    updated my thoughts on ships and ships_test

--- lib/ships.rb
+++ lib/ships.rb
@@ -1,15 +1,24 @@
 require 'pry'
 
 class Ships
-  attr_reader :length, :positions
-  def initialize(length, positions)
+  attr_reader :length, :ship_location
+
+  def initialize(length, ship_location)
     @length = length
-    @positions = [a,b]
+    @ship_location = []
   end
 
-
-
   def place_ships
-    #a doctor specializes in indian food
+    puts "Prepare for battle! Place your DESTROYER."
+    print "> "
+    slot_1 = $stdin.gets.chomp
+      if slot_1 == A1..D4
+        @ship_location << slot_1
+      end
+    slot_2 = $stdin.gets.chomp
+      if slot_2 == A1..D4 && slot_2.include? != slot_1 #prevents ship stacking
+        @ship_location << slot_2
+      end
+    @ship_location
   end
 end
