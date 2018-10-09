887320e45d3a1edbca4388ccf162d373386f794f
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-09 12:53:55 -0600

    built a troll interface for the introduction menu

--- lib/ships.rb
+++ lib/ships.rb
@@ -1,22 +1,15 @@
 require 'pry'
+
 class Ships
   attr_reader :length, :positions
   def initialize(length, positions)
     @length = length
-    @positions = []
+    @positions = [a,b]
   end
 
 
 
   def place_ships
-    slot_1 = [@positions.first]
-    slot_2 = [@positions.last]
-    # slot_1 = [:key]
-    # slot_2 = [:value]
-    slots = [slot_1, slot_2]
-    puts slots
-    # @positions << slots
-    # @positions
+    
   end
-  # binding.pry
 end
