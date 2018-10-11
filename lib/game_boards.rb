7fe4097ac9b59347784d70ccf4388c4c56573da0
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-11 14:53:57 -0600

    Merge branch 'master' of github.com:holt-ace/battleship-bruh into Michael_1

--- lib/game_boards.rb
+++ lib/game_boards.rb
@@ -4,11 +4,35 @@ class Game_Boards
   def print_board#(player or computer)
   end
 
+  @hash_chart = {
+                     A1: S,
+                     A2: S,
+                     A3: A3,
+                     A4: A4,
+                     B1: B1,
+                     B2: B2,
+                     B3: B3,
+                     B4: B4,
+                     C1: C1,
+                     C2: C2,
+                     C3: C3,
+                     C4: C4,
+                     D1: D1,
+                     D2: D2,
+                     D3: D3,
+                     D4: D4
+                   }
+»»
   def empty_board
-    row_1 = "=" * 11
-    row_2 = "." + "1" + "2" + "3" + "4"
-    single_space = " "
-    big_space = "   "
+    row_1 = "=" * 20
+    row_2 =
+    row_3 = [@hash_chart[A1], @hash_chart[A2], @hash_chart[A3], @hash_chart[A4]]
+    row_4 = [@hash_chart[B1], etc]
+    row_5 =
+    row_6 = 
+    row_7 = row_1
+
   end
+
 
 end
