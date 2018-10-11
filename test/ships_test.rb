edbcaada16ba5a82324d2b69731b5e3c07caae27
Asa Holt <holt.ace@icloud.com>
2018-10-11 09:31:22 -0600

    Fixes ships_test

--- test/ships_test.rb
+++ test/ships_test.rb
@@ -5,18 +5,23 @@ require './lib/ships'
 
 class ShipsTest < Minitest::Test
   def setup
-
     @destroyer = Ships.new(3, [:A1, :A3])
     @canoe = Ships.new(2, [:D3, :D4])
   end
 
-  def test_if_destroyer_exists
-
+  def test_if_ships_exists
+    assert_instance_of Ships, @canoe
     assert_instance_of Ships, @destroyer
   end
 
-  def test_if_the_destroyer_is_placed
+  def test_the_ships_have_correct_length
+    assert_equal 3, @destroyer.length
+    assert_equal 2, @canoe.length
 
-    assert_equal ["A1", "A2", "A3"], @destroyer.position
+  end
+
+  def test_if_the_destroyer_is_placed
+    assert_equal [:A1, :A3], @destroyer.ship_location
+    assert_equal [:D3, :D4], @canoe.ship_location
   end
 end
