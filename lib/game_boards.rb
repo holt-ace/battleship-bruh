81ff8527c0f4450df02ca233c6c873693f31b0f1
Asa Holt <holt.ace@icloud.com>
2018-10-12 10:24:52 -0600

    game_board additions

--- lib/game_boards.rb
+++ lib/game_boards.rb
@@ -22,12 +22,12 @@ class Game_Boards
                      D3: D3,
                      D4: D4
                    }
-»»
+
   def empty_board
     row_1 = "=" * 20
     row_2 =
-    row_3 = [@hash_chart[A1], @hash_chart[A2], @hash_chart[A3], @hash_chart[A4]]
-    row_4 = [@hash_chart[B1], etc]
+    row_3 = [@hash_chart[:A1], @hash_chart[:A2], @hash_chart[:A3], @hash_chart[:A4]]
+    row_4 = [@hash_chart[:B1], etc]
     row_5 =
     row_6 =
     row_7 = row_1
@@ -35,7 +35,48 @@ class Game_Boards
   end
 
   def placement_rules
-    if 
+    #conditionals that eliminate the player from placing his ships-
+    #in an overlapping, wrapping around the board, or physically  -
+    #impossible fashion
+     if player_entry == []
 
+     end
+  end
+
+  def store_hash_values
+    if player_entry_for_destroyer == [:A1, :A3]
+      @hash_chart.store(:A1..:A3, "S")
+      #stores a new key:value pair in the hash
+      #if this ^^^ works I will be stoked. not sure if you can take-
+      #a range as an argument
+    elsif player_entry_for_destroyer == [:A2, :A4]
+      @hash_chart.store(:A2..:A4, "S")
+    elsif player_entry_for_destroyer == [:B1, :B3]
+      @hash_chart.store(:B1..:B3, "S")
+    elsif player_entry_for_destroyer == [:B2, :B4]
+      @hash_chart.store(:B2..:B4, "S")
+    elsif player_entry_for_destroyer == [:C1, :C3]
+      @hash_chart.store(:C1..:C3, "S")
+    elsif player_entry_for_destroyer == [:C2, :C4]
+      @hash_chart.store(:C2..:C4, "S")
+    elsif player_entry_for_destroyer == [:D1, :D3]
+      @hash_chart.store(:D1..:D3, "S")
+    elsif player_entry_for_destroyer == [:D2, :D4]
+      @hash_chart.store(:D2..:D4, "S")
+
+      #only covers horizontal placement^^^^^
+      #had a hard time thinking about how to do vertical
+
+      #may need to use .rehash method after changing key:value pairs?
+
+      #the .rehash method: Rebuilds the hash based on the current hash-
+      #values for each key. If values of key objects have changed since-
+      #they were inserted, this method will reindex hsh. If Hash#rehash-
+      #is called while an iterator is traversing the hash, a RuntimeError-
+      #will be raised in the iterator.
+
+
+    end
+  end
 
 end
